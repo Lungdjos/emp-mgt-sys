8e481f06a90ebc7df73a9f0bd7d64ea81fa107ab
Lungu David <lungudjos@gmail.com>
2024-10-29 15:49:18 +0200

    adding db configs

---
+++ empmgt/src/main/java/com/radix/ems/EmpmgtApplication.java
@@ -0,0 +1,13 @@
+package com.radix.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmgtApplication.class, args);
+	}
+
+}
