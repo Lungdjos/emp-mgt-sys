ef28a5789fefad5a6c963168123bd1cd4f1087fe
Lungu David <lungudjos@gmail.com>
2024-10-29 16:59:15 +0200

    rest api for creating a new employee

--- empmgt/src/main/java/com/radix/ems/controller/EmployeeRestCtrl.java
+++ empmgt/src/main/java/com/radix/ems/controller/EmployeeRestCtrl.java
@@ -1,9 +1,31 @@
 package com.radix.ems.controller;
 
+import com.radix.ems.dto.EmployeeDto;
+import com.radix.ems.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeRestCtrl {
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     *
+     * @param employeeDto
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(EmployeeDto employeeDto) {
+        try {
+            return  new ResponseEntity<>(employeeService.createEmployee(employeeDto), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.FOUND);
+        }
+    }
 }
