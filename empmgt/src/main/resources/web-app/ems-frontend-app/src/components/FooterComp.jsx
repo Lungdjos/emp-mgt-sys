534f475896306a7c63ca8fa1107b8cb28f81e017
Lungu David <lungudjos@gmail.com>
2024-11-01 11:47:54 +0200

    adding the footer and it's css

---
+++ empmgt/src/main/resources/web-app/ems-frontend-app/src/components/FooterComp.jsx
@@ -0,0 +1,8 @@
+const FooterComp = () => {
+    return (
+        <footer className="footer bg-dark text-center" data-bs-theme="dark">
+        <p>All Rights Reserved by &copy; 2024 Radix Enterprise Ltd.</p>
+    </footer>);
+}
+
+export default FooterComp
