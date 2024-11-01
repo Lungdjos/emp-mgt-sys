97e3ead465927480318963af8394199839418c9b
Lungu David <lungudjos@gmail.com>
2024-11-01 11:48:53 +0200

    adding the header

---
+++ empmgt/src/main/resources/web-app/ems-frontend-app/src/components/HeaderComp.jsx
@@ -0,0 +1,15 @@
+const HeaderComp = () => {
+    return (
+        <>
+            <header>
+                <nav className="text-center navbar" style={{ backgroundColor: '#e3f9fd' }}>
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Employee Management System</a>
+                    </div>
+                </nav>
+                </header>
+        </>
+    )
+}
+
+export default HeaderComp
