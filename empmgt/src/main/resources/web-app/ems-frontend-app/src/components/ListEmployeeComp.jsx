b78249845f1243ae1c76f573fe8fcdab33bf55d5
Lungu David <lungudjos@gmail.com>
2024-11-01 14:53:15 +0200

    some code refactoring

--- empmgt/src/main/resources/web-app/ems-frontend-app/src/components/ListEmployeeComp.jsx
+++ empmgt/src/main/resources/web-app/ems-frontend-app/src/components/ListEmployeeComp.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const ListEmployeeComp = () => {
 
-    const [employees, setListEmployee] = useState([])
+    const [employees, setListEmployee] = useState([]);
 
+    const navigate = useNavigate();
+
+    // js method to get all employees
     const getAllEmployees = () => {
         return axios.get("http://localhost:8080/api/v1/employees");
     }
@@ -17,12 +21,20 @@ const ListEmployeeComp = () => {
         })
     }, [])
 
+    // js method to addEmployee
+    const addEmployee = () => {
+        navigate("/add-employee");
+    }
+
 
     return (
         <>
             <div className="container">
                 <h2 className="text-center">List of Employees</h2>
-                <table className="table">
+                <div className="r-grid gap-1">
+                  <button className="btn btn-primary btn-lg" type="button" onClick={addEmployee}>Add New Employee</button>
+                </div>
+                <table className="table table-striped table-sm">
                   <thead>
                     <tr>
                       <th scope="col">No.</th>
@@ -48,8 +60,8 @@ const ListEmployeeComp = () => {
                               <td>{employee.department}</td>
                               <td>{employee.designation}</td>
                               <td>
-                                  <button className="btn btn-primary">Edit</button>
-                                  <button className="btn btn-danger">Delete</button>
+                                  <button className="btn btn-primary me-2">Edit</button>
+                                  <button className="btn btn-danger"><i className="bi bi-trash"></i>Delete</button>
                               </td>
                           </tr>
                       ))}
